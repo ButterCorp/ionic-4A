81ddd43699cb118517ce0d9a2941993bd1d45cd2
Kevin <kev_kevin@hotmail.fr>
2018-06-27 12:38:36 +0200

    implement searchbar

--- src/pages/launch-list/launch-list.ts
+++ src/pages/launch-list/launch-list.ts
@@ -19,10 +19,16 @@ import { LaunchDetailsPage } from '../launch-details/launch-details';
 export class LaunchListPage {
   private launches: any;
   private year: any;
+  private searchQuery: string = '';
+  private launches_filter: any;
+  items: string[];
+
   constructor(
     public navCtrl: NavController, 
     public navParams: NavParams, 
-    private spacexApi: SpacexApiProvider
+    private spacexApi: SpacexApiProvider,
+    
+
   ) {
       spacexApi.getAllLaunches(
         {
@@ -30,12 +36,16 @@ export class LaunchListPage {
         }
       ).subscribe(data => {
         this.launches = data;
+        this.launches_filter = this.launches;
       })
       if(typeof navParams.data == "string"){
         this.year = navParams.data;
       }
  
-  
+    }
+
+    initializeItems() {
+      this.launches_filter = this.launches;
     }
     
 
@@ -45,5 +55,22 @@ export class LaunchListPage {
   goToDetail(launch: any) {
     this.navCtrl.push(LaunchDetailsPage, launch);
   }
+
+  getItems(ev: any) {
+    // Reset items back to all of the items
+    this.initializeItems();
+
+    // set val to the value of the searchbar
+    const val = ev.target.value;
+
+    //if the value is an empty string don't filter the items
+    if (val && val.trim() != '') {
+      this.launches_filter = this.launches_filter.filter((item) => {
+        return (item.mission_name.toLowerCase().indexOf(val.toLowerCase()) > -1);
+      })
+    }
+  }
+
+  
 
 }
