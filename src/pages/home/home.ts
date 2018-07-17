0c3af8ccc4c23a62da3b61e5d92114907686b5ad
Kevin <kev_kevin@hotmail.fr>
2018-07-17 16:33:02 +0200

    create countdown for next launch

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -12,6 +12,9 @@ export class HomePage {
 
   nextLaunch: ILaunch;
   nextLaunches: ILaunch[];
+  launchTime: any;
+  interval: any;
+  launchTimeCountDown: any;
 
   constructor(
     public navCtrl: NavController,
@@ -23,8 +26,12 @@ export class HomePage {
 
   getNextLaunch(): void {
     this.spacexApi.getNextLaunch()
-        .subscribe(data => { this.nextLaunch = data; });
+        .subscribe(data => { this.nextLaunch = data;
+                            this.launchTime = data.launch_date_utc; 
+                            this.initRefreshCountDown();
+          })
   }
+
 
   getNextLaunches(): void {
     this.spacexApi.getNextLaunches()
@@ -34,4 +41,40 @@ export class HomePage {
   goToDetail(launch: ILaunch): void {
     this.navCtrl.push(LaunchDetailsPage, launch);
   }
+ 
+  ionViewDidLeave() {
+    this.stopRefresh();
+  }
+
+  initRefreshCountDown() {
+    this.refresh();
+    this.interval = setInterval(() => this.refresh(), 1000);
+  }
+
+  stopRefresh() {
+    clearInterval(this.interval);
+  }
+
+  refresh() {
+    const currentDate = new Date().getTime();
+    const timeToLaunchTime = new Date(this.launchTime).getTime() - currentDate;
+    let day, hour, minute, seconds;
+    seconds = Math.floor(timeToLaunchTime / 1000);
+    minute = Math.floor(seconds / 60);
+    seconds = seconds % 60;
+    hour = Math.floor(minute / 60);
+    minute = minute % 60;
+    day = Math.floor(hour / 24);
+    hour = hour % 24;
+
+    const result = {
+      day: day,
+      hour: hour,
+      minute: minute,
+      seconds: seconds
+    };
+
+    this.launchTimeCountDown = result;
+  }
 }
+
